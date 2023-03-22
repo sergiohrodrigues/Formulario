5752a5cc4394896c421fd782e1d8cfd805b58ebc
Sergio <sergiohrodriguess@gmail.com>
2023-03-22 16:11:35 -0300

    sucesso active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let btnSubmit = document.querySelector('#btn');
 let btns = document.querySelectorAll('.form__item')
 let erroParag = document.querySelectorAll('#erroParag');
+const sucess = document.querySelector("#sucess")
 
 btnSubmit.addEventListener('click', verificarCampoInvalido)
 
@@ -8,12 +9,14 @@ function verificarCampoInvalido(event) {
     event.preventDefault();
 
     for (let i = 0; i < btns.length; i++) {
-        if (btns[i].value === '') {
-            btns[i].classList.add('form__item-img');
-            erroParag[i].style.display = 'Block';
-        } else {
+        if (btns[i].value !== '') {
             btns[i].classList.remove('form__item-img');
             erroParag[i].style.display = 'none';
+            sucess.style.display = "block"
+            btns[i].value = ""
+        } else {
+            btns[i].classList.add('form__item-img');
+            erroParag[i].style.display = 'Block';
         }
     }
 }
